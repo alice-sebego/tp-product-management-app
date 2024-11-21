d6a0bde4ee11ff646c0ee3c997faeac8559aed5d
Alice Sebego <alice.etvotrecom@gmail.com>
2024-11-21 16:25:23 +0100

    feat: create and initialize database on sqlserver

---
+++ Main.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Data;
+
+class MainClass {
+
+    static string connectionString = "Server=localhost;Database=SportsCatalog;Trusted_Connection=True;";
+    public static void Main (string[] args) {
+        try {
+            // Initialize the database
+            Init.InitializeDatabase();
+            Console.WriteLine("La base de données a été initialisée avec succès.");
+        }
+        catch (Exception e) {
+            Console.WriteLine($"Erreur lors de l'initialisation de la base de données : {e.Message}");
+            return;
+        }
+    }
+}
